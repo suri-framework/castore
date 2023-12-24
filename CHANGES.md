453863fd6325121a7c059bd3844e00fa119a0f7a
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-24 18:40:08 +0100

    docs: prepare for 0.0.1

---
+++ CHANGES.md
@@ -0,0 +1,5 @@
+# Changes
+
+## 0.0.1
+
+Initial release of CAStore including a PEM file generated on Dec 12.
---
+++ ca_store.mli
@@ -0,0 +1,2 @@
+val certificate : [> `Ca_contents of string ]
+(** An automatically generated Certificate file to be used with libraries like [X509]. *)
